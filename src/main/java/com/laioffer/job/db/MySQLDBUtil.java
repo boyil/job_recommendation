e54f0cd81ff9e76e5d6b6ec39423de8a8e62eba4
boyil <boyi.l.dimi@gmail.com>
2020-09-06 14:58:33 -0700

    Hide private information

--- src/main/java/com/laioffer/job/db/MySQLDBUtil.java
+++ src/main/java/com/laioffer/job/db/MySQLDBUtil.java
@@ -1,11 +1,11 @@
 package com.laioffer.job.db;
 
 public class MySQLDBUtil {
-    private static final String INSTANCE = "mydbinstance.c6iacvjdxysg.us-west-1.rds.amazonaws.com";
+    private static final String INSTANCE = "[My db instance endpoint]";
     private static final String PORT_NUM = "3306";
-    public static final String DB_NAME = "mydb";
-    private static final String USERNAME = "admin";
-    private static final String PASSWORD = "Password681";
+    public static final String DB_NAME = "[db name]";
+    private static final String USERNAME = "[username]";
+    private static final String PASSWORD = "[password]";
     public static final String URL = "jdbc:mysql://"
             + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
             + "?user=" + USERNAME + "&password=" + PASSWORD
